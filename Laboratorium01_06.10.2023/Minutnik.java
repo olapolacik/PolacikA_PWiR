17449484f90559510db1802955178107639c814e
Aleksandra Połacik <polacik.ola@gmail.com>
2023-10-07 10:57:09 +0200

    minutnik

--- Laboratorium01_06.10.2023/Minutnik.java
+++ Laboratorium01_06.10.2023/Minutnik.java
@@ -1,28 +1,62 @@
-import javax.swing.*;
-import java.awt.*;
+import java.util.Scanner;
 
 public class Minutnik {
     public static void main(String[] args) {
-        // Tworzenie ramki głównej
-        JFrame frame = new JFrame("Przykład GUI Panelu");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(400, 300);
 
-        // Tworzenie panelu
-        JPanel panel = new JPanel();
-        panel.setLayout(new FlowLayout());
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Podaj ilosc sekund: ");
 
-        // Dodawanie komponentów do panelu
-        JLabel label = new JLabel("Witaj, to jest etykieta!");
-        JButton button = new JButton("To jest przycisk");
+        // czas w sekundach podany przez uzytkownika
+        int totalTimeInSeconds = sc.nextInt(); 
+        
+        // Tworzenie i uruchamianie wątków minutników
+        TimerThread realTimeTimer = new TimerThread("Minutnik w czasie rzeczywistym", totalTimeInSeconds);
+        TimerThread delayedTimer = new TimerThread("Minutnik z opóźnieniem", totalTimeInSeconds, 2000);
 
-        panel.add(label);
-        panel.add(button);
+        realTimeTimer.start();
+        delayedTimer.start();
+    }
+}
 
-        // Dodawanie panelu do ramki
-        frame.add(panel);
+class TimerThread extends Thread {
+    private String name;
+    private int totalTimeInSeconds;
+    private int delay;
 
-        // Wyświetlanie ramki
-        frame.setVisible(true);
+    public TimerThread(String name, int totalTimeInSeconds) {
+        this.name = name;
+        this.totalTimeInSeconds = totalTimeInSeconds;
+        this.delay = 0;
+    }
+
+    public TimerThread(String name, int totalTimeInSeconds, int delay) {
+        this.name = name;
+        this.totalTimeInSeconds = totalTimeInSeconds;
+        this.delay = delay;
+    }
+
+    @Override
+    public void run() {
+        System.out.println(name + " rozpoczyna odliczanie...");
+        int remainingTime = totalTimeInSeconds;
+
+        try {
+            Thread.sleep(delay); // Opcjonalne opóźnienie
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        while (remainingTime > 0) {
+            System.out.println(name + ": " + remainingTime + " sek.");
+            remainingTime--;
+
+            try {
+                Thread.sleep(1000); // Czekaj 1 sekundę
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        System.out.println(name + " zakończył odliczanie.");
     }
 }
